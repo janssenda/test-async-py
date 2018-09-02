31c27d9b49a55a98209cbcba4ed2d9b35510b3fc
Danimae Janssen <janssenda@gmail.com>
2018-09-01 22:39:17 -0500

    automated thread management

--- complex-process.py
+++ complex-process.py
@@ -6,27 +6,26 @@ import cv2
 import numpy as np
 
 def sub(obj, ROI):
-
-    print("Thread before\t" + str(ROI[0,0]))
     np.copyto(ROI[:,:,0],obj.apply(ROI, None, -1))
 
-    # for i in range(len(ROI_new)):
-    #     for j in range(len(ROI_new[i])):
-    #         ROI[i,j] = ROI_new[i,j]
-
-    print("Thread after\t" + str(ROI[0,0]))
-
-
 fgbg = cv2.createBackgroundSubtractorKNN(100, 500, False)
-ROI = (255*np.random.rand(320,200,1)).astype(dtype="uint8")
 
-print("Main before\t" + str(ROI[0,0]))
+threadcount = 10
+threads = []
+data = []
 
-t1 = threading.Thread(target=sub, args=[fgbg, ROI])
-t1.start()
-t1.join()
+for x in range(threadcount):
+    data.append((255 * np.random.rand(320, 200, 1)).astype(dtype="uint8"))
+    threads.append(threading.Thread(target=sub, args=[fgbg, data[x]]))
 
-print(ROI.shape)
+# Start them all
+for thread in threads:
+    thread.start()
 
-print("Main after\t" + str(ROI[0,0]))
+# Wait for all to complete
+for thread in threads:
+    thread.join()
+
+print()
+
 

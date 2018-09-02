9e2da35b0dcf1c4f9cafc5f6aaf8521e681964f2
Danimae Janssen <janssenda@gmail.com>
2018-09-01 21:58:53 -0500

    copytoooo

--- complex-process.py
+++ complex-process.py
@@ -6,8 +6,14 @@ import cv2
 import numpy as np
 
 def sub(obj, ROI):
+
     print("Thread before\t" + str(ROI[0,0]))
-    ROI.put(ROI,obj.apply(ROI, None, -1))
+    np.copyto(ROI[:,:,0],obj.apply(ROI, None, -1))
+
+    # for i in range(len(ROI_new)):
+    #     for j in range(len(ROI_new[i])):
+    #         ROI[i,j] = ROI_new[i,j]
+
     print("Thread after\t" + str(ROI[0,0]))
 
 
@@ -19,6 +25,8 @@ print("Main before\t" + str(ROI[0,0]))
 t1 = threading.Thread(target=sub, args=[fgbg, ROI])
 t1.start()
 t1.join()
+
+print(ROI.shape)
 
 print("Main after\t" + str(ROI[0,0]))
 
